45551843333a05a351c6d686ded43fdf0cc08b35
lashasaata <lashasaata@gmail.com>
2024-09-13 13:28:12 -0700

    install hook-forms, axios and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import axios from "axios";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return <></>;
