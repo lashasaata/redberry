bf233bd24df7e0a2611eb902468f9f7318c12f21
lashasaata <lashasaata@gmail.com>
2024-09-14 06:31:19 -0700

    style: styles for filter bar

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,36 +4,62 @@ function Landing() {
       <div className="px-[162px] py-[38px] border-b border-solid border-[#dbdbdb]">
         <img src="/redberry logo.png" alt="logo" />
       </div>
-      <nav>
-        <section>
-          <div>
-            <span>რეგიონი</span>
-            <img src="/icon-arrow-down.svg" alt="arrow-down" />
-          </div>
-          <div>
-            <span>საფასო კატეგორია</span>
-            <img src="/icon-arrow-down.svg" alt="arrow-down" />
-          </div>
-          <div>
-            <span>ფართობი</span>
-            <img src="/icon-arrow-down.svg" alt="arrow-down" />
-          </div>
-          <div>
-            <span>საძინებლების რაოდენობა</span>
-            <img src="/icon-arrow-down.svg" alt="arrow-down" />
-          </div>
-        </section>
-        <section>
-          <button>
-            <img src="/ic-round-plus.png" alt="plus" />
-            <span>ლისტინგის დამატება</span>
-          </button>
-          <button>
-            <img src="ic-round-plus.png" alt="" />
-            <span>აგენტის დამატება</span>
-          </button>
-        </section>
-      </nav>
+      <div className="w-full px-[162px]">
+        <nav className="flex items-center justify-between">
+          <section className="flex items-center gap-6 p-[6px] rounded-[10px] border border-solid border-[#dbdbdb]">
+            <div className="px-[14px] py-2 flex items-center gap-1 rounded-[6px]">
+              <span className="text-base text-[#021526] font-[600]">
+                რეგიონი
+              </span>
+              <img
+                className="mt-1"
+                src="/icon-arrow-down.svg"
+                alt="arrow-down"
+              />
+            </div>
+            <div className="px-[14px] py-2 flex items-center gap-1 rounded-[6px]">
+              <span className="text-base text-[#021526] font-[600]">
+                საფასო კატეგორია
+              </span>
+              <img
+                className="mt-1"
+                src="/icon-arrow-down.svg"
+                alt="arrow-down"
+              />
+            </div>
+            <div className="px-[14px] py-2 flex items-center gap-1 rounded-[6px]">
+              <span className="text-base text-[#021526] font-[600]">
+                ფართობი
+              </span>
+              <img
+                className="mt-1"
+                src="/icon-arrow-down.svg"
+                alt="arrow-down"
+              />
+            </div>
+            <div className="px-[14px] py-2 flex items-center gap-1 rounded-[6px]">
+              <span className="text-base text-[#021526] font-[600]">
+                საძინებლების რაოდენობა
+              </span>
+              <img
+                className="mt-1"
+                src="/icon-arrow-down.svg"
+                alt="arrow-down"
+              />
+            </div>
+          </section>
+          <section>
+            <button>
+              <img src="/ic-round-plus.png" alt="plus" />
+              <span>ლისტინგის დამატება</span>
+            </button>
+            <button>
+              <img src="ic-round-plus.png" alt="" />
+              <span>აგენტის დამატება</span>
+            </button>
+          </section>
+        </nav>
+      </div>
     </main>
   );
 }
