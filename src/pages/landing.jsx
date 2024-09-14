2b26b924c106360099f905d16857916fc589054a
lashasaata <lashasaata@gmail.com>
2024-09-14 06:03:46 -0700

    add filter-add sections with elements and content

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,6 +4,36 @@ function Landing() {
       <div className="px-[162px] py-[38px] border-b border-solid border-[#dbdbdb]">
         <img src="/redberry logo.png" alt="logo" />
       </div>
+      <nav>
+        <section>
+          <div>
+            <span>რეგიონი</span>
+            <img src="/icon-arrow-down.svg" alt="arrow-down" />
+          </div>
+          <div>
+            <span>საფასო კატეგორია</span>
+            <img src="/icon-arrow-down.svg" alt="arrow-down" />
+          </div>
+          <div>
+            <span>ფართობი</span>
+            <img src="/icon-arrow-down.svg" alt="arrow-down" />
+          </div>
+          <div>
+            <span>საძინებლების რაოდენობა</span>
+            <img src="/icon-arrow-down.svg" alt="arrow-down" />
+          </div>
+        </section>
+        <section>
+          <button>
+            <img src="/ic-round-plus.png" alt="plus" />
+            <span>ლისტინგის დამატება</span>
+          </button>
+          <button>
+            <img src="ic-round-plus.png" alt="" />
+            <span>აგენტის დამატება</span>
+          </button>
+        </section>
+      </nav>
     </main>
   );
 }
